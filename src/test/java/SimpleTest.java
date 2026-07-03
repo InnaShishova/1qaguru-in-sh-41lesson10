d16618849bd560a2bd0592c0a478bc6064c57dc6
Inna Shishova <sh201608@yandex.ru>
2026-07-03 19:08:30 +0400

    added gitignore

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    @Owner("Inna Shishova")
+    @Feature("Jenkins homework")
+    @Story("Simple autotest")
+    @DisplayName("Successful simple test")
+    void successfulTest() {
+        step("Проверяем, что тест запускается", () -> {
+            assertTrue(5 > 1);
+        });
+    }
+
+    @Step("{stepName}")
+    void step(String stepName, Runnable action) {
+        action.run();
+    }
+}
